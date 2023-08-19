4e1e0c8b29cdb9035b6cf66e9d08ef6b9d98d3c6
osho81 <ossi.hour@gmail.com>
2023-08-19 12:57:08 +0200

    Change master to main

--- src/main/java/com/fmellberg/economyapp/EconomyAppBackendApplication.java
+++ src/main/java/com/fmellberg/economyapp/EconomyAppBackendApplication.java
@@ -3,6 +3,8 @@ package com.fmellberg.economyapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Git comment to make a push with new master branch name: main
+
 @SpringBootApplication
 public class EconomyAppBackendApplication {
 
