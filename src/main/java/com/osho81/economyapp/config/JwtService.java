774f3d2ece27ad071fb5581cfe68e123b3504121
osho81 <ossi.hour@gmail.com>
2023-09-10 15:03:45 +0200

    Add JwtService

--- src/main/java/com/osho81/economyapp/config/JwtService.java
+++ src/main/java/com/osho81/economyapp/config/JwtService.java
@@ -1,5 +1,7 @@
 package com.osho81.economyapp.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 // Service class for extracting userName/email from token
@@ -10,5 +12,15 @@ public class JwtService {
     // From a
     public String extractUsername(String token) {
         return null;
+    }
+
+    // Extract jwt claims
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws() // jws = json web signature
+                .getBody(); // parse the token and get it
     }
 }
