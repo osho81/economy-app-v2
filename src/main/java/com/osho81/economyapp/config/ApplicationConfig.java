68a19bb0e2e4fcfc3f9ba1e146225b71c48e6fa9
osho81 <ossi.hour@gmail.com>
2023-09-10 22:25:35 +0200

    Add ApplicationConfig

---
+++ src/main/java/com/osho81/economyapp/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.osho81.economyapp.config;
+
+import com.osho81.economyapp.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        // Use customized crud method in user repo
+        return username -> userRepository.findByEmail(username)
+                // Retrns optional; so handle exception:
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
