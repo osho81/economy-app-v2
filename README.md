3cb28f74196399f027a0dd8052d54b037182ddf9
osho81 <ossi.hour@gmail.com>
2023-09-17 22:49:57 +0200

    Add SecurityConfiguration.java

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Economy-app-v2
 Similar as economy-app (version 1), with following changes/additions:
-- Change db from H2 to ?
+- Change db from H2 to MySQL
 - to be filled in soon
 
 ## EconomyApp-Backend
--- src/main/java/com/osho81/economyapp/config/SecurityConfiguration.java
+++ src/main/java/com/osho81/economyapp/config/SecurityConfiguration.java
@@ -1,4 +1,43 @@
 package com.osho81.economyapp.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Spring security v 3
+
+// Responsible for all http security in the app
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+
+                // Create a white list
+                .requestMatchers("urls")
+                .permitAll()
+
+                // All else must be authenticated
+                .anyRequest()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+
+        return http.build();
+
+    }
+
 }
